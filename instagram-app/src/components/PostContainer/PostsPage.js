035ff35c903a068ea559be5a0648b2db610a8faf
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-06-06 17:53:17 -0400

    Implement fuzzy search for post and comment username

--- instagram-app/src/components/PostContainer/PostsPage.js
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import dummyData from '../../dummy-data';
 import PostContainer from './PostContainer'
 import SearchBar from '../SearchBar/SearchBar';
+import Fuse from 'fuse.js'
 
 class PostsPage extends Component {
     state = {
@@ -76,18 +77,17 @@ class PostsPage extends Component {
 
     searchPost = (event) => {
         this.setState({
-            searchTerm: event.target.value
+            searchTerm: event.target.value,
         })
     }
-    
     render() {
+        const fuse = new Fuse(this.state.posts, options)
         return (
             <>
                 <SearchBar search = {this.searchPost} searchTerm = {this.state.searchTerm}/>
                 <PostContainer
                     searchTerm = {this.state.searchTerm} 
-                    posts = {this.state.posts.filter(
-                        post => post.username.toLowerCase().includes(this.state.searchTerm.toLowerCase()))} 
+                    posts = {this.state.searchTerm==='' ? this.state.posts : fuse.search(this.state.searchTerm)}
                     liker = {this.liker} 
                     addNewComment = {this.addNewComment}
                     commentInput = {this.commentInput}
@@ -99,5 +99,21 @@ class PostsPage extends Component {
         );
     }
 }
+
+
+const options = {
+    shouldSort: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "username",
+        "comments.username"
+    ]
+};
+
+
 
 export default PostsPage
